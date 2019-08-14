1347669d9fb4f2ce74dcb61d1402e84d2b691fcc
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2019-08-14 12:15:43 -0300

    Add lambda that creates pyspark cluster to submit partition script

---
+++ aws/lambda/submitPartitionRoutineCluster/RADME.md
@@ -0,0 +1,20 @@
+### Description
+
+This function must be triggered hourly by an CloudWatch Rules and has the aim of create a cluster and submit a pyspark task.
+This pyspark task is intended to partition stream data by creation or update date.
+
+
+### Execution Role
+
+There is an role called VtexDatalakeLambdaEC2Access with the following polices:
+    - AmazonEC2FullAccess
+    - AWS managed policy
+    - Log policies
+
+
+### Env vars
+This lambda needs these vars:
+    - LOG_DEST_S3_URI
+        * s3 uri where save logs. ex: s3://aws-logs
+    - SCRIPT_S3_URI
+        * s3 uri where is located the script to be submitted, ex: s3://vtex.datalake/scripts/script.py
---
+++ aws/lambda/submitPartitionRoutineCluster/lambda_function.py
@@ -0,0 +1,84 @@
+import logging
+import boto3
+import os
+import time
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(getattr(logging, os.getenv('LOG_LEVEL', 'INFO')))
+
+# AWS Client
+emr_client = boto3.client('emr', region_name='us-east-2')
+
+# ENV variables
+SCRIPT_S3_URI = os.getenv('SCRIPT_S3_URI') 
+LOG_DEST_S3_URI = os.getenv('LOG_DEST_S3_URI')
+
+SCRIPT_NAME = SCRIPT_S3_URI.split('/')[-1]
+HOME_HADOOP = '/home/hadoop/'
+
+
+def lambda_handler(event, context):
+    cluster_id = emr_client.run_job_flow(
+        Name='PartitionRoutine | ' + time.strftime("%Y/%m/%d %H:%M:%S"),
+        ReleaseLabel='emr-5.18.0',
+        Applications=[
+            {
+                'Name': 'Spark'
+            },
+        ],
+        Instances={
+            'InstanceGroups': [
+                {
+                    'Name': "Master nodes",
+                    'Market': 'ON_DEMAND',
+                    'InstanceRole': 'MASTER',
+                    'InstanceType': 'r5.xlarge',
+                    'InstanceCount': 1,
+                },
+                {
+                    'Name': "Slave nodes",
+                    'Market': 'ON_DEMAND',
+                    'InstanceRole': 'CORE',
+                    'InstanceType': 'r5.xlarge',
+                    'InstanceCount': 1,
+                },
+                {
+                    'Name': "Slave nodes",
+                    'Market': 'ON_DEMAND',
+                    'InstanceRole': 'TASK',
+                    'InstanceType': 'r5.xlarge',
+                    'InstanceCount': 2,
+                },            
+            ],
+            'Ec2KeyName': 'dev_datalake',
+            'KeepJobFlowAliveWhenNoSteps': False,
+            'TerminationProtected': False,
+            'Ec2SubnetId': 'subnet-11e84e5d',
+        },        
+        Steps=[
+            {
+                'Name': 'Get pyspark script',
+                'ActionOnFailure': 'TERMINATE_CLUSTER',
+                'HadoopJarStep': {
+                    'Jar': 'command-runner.jar',
+                    'Args': ['aws', 's3', 'cp', SCRIPT_S3_URI, HOME_HADOOP]
+                }
+            },
+            {
+                'Name': 'Run pyspark script',
+                'ActionOnFailure': 'CONTINUE',
+                'HadoopJarStep': {
+                    'Jar': 'command-runner.jar',
+                    'Args': ['spark-submit', HOME_HADOOP + SCRIPT_NAME]
+                }
+            }
+        ],            
+        JobFlowRole='EMR_EC2_DefaultRole',
+        ServiceRole='EMR_DefaultRole',
+        VisibleToAllUsers=True,
+        LogUri=LOG_DEST_S3_URI,
+    )
+    
+    logger.info('Cluster {} created with the step.'.format(cluster_id['JobFlowId']))
+    
