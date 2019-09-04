fd5f2fb72e69a55d29717709f7e04bb0ce95afb6
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2019-09-04 13:45:29 -0300

    Loading and creating cluster with custom confs for History Partition

--- scripts/checkout_partition_history/submit_cluster_with_spark_partition_jobs.py
+++ scripts/checkout_partition_history/submit_cluster_with_spark_partition_jobs.py
@@ -2,6 +2,8 @@ import logging
 import boto3
 import os
 import time
+import json
+import argparse
 
 from dotenv import load_dotenv
 
@@ -10,15 +12,16 @@ from dotenv import load_dotenv
 load_dotenv('.env')
 SCRIPT_S3_URI = os.getenv('SCRIPT_S3_URI')
 LOG_DEST_S3_URI = os.getenv('LOG_DEST_S3_URI')
-DESTINATION_S3_URI_PREFIX = os.getenv('DESTINATION_S3_URI_PREFIX')
 
 
 logging.basicConfig()
 logger = logging.getLogger(__name__)
 logger.setLevel(getattr(logging, os.getenv('LOG_LEVEL', 'INFO')))
 
+
 # AWS Client
 emr_client = boto3.client('emr', region_name='us-east-2')
+
 
 SCRIPT_NAME = SCRIPT_S3_URI.split('/')[-1]
 HOME_HADOOP = '/home/hadoop/'
@@ -60,15 +63,15 @@ def get_instances_descriptions():
                 'Name': "Master nodes",
                 'Market': 'ON_DEMAND',
                 'InstanceRole': 'MASTER',
-                'InstanceType': 'm4.2xlarge',
+                'InstanceType': 'r5.2xlarge',
                 'InstanceCount': 1,
             },  
             {
                 'Name': "Slave nodes",
                 'Market': 'ON_DEMAND',
                 'InstanceRole': 'CORE',
-                'InstanceType': 'r5.xlarge',
-                'InstanceCount': 1,
+                'InstanceType': 'c5.4xlarge',
+                'InstanceCount': 2,
             }               
         ],
         'Ec2KeyName': 'dev_datalake',
@@ -78,7 +81,9 @@ def get_instances_descriptions():
     }
  
 
-def submit_cluster(cluster_seq):
+def submit_cluster(cluster_seq, config_from_json):
+    configurations = config_from_json or []
+
     cluster_id = emr_client.run_job_flow(
         Name='Partition History --- ' + cluster_seq + "*_CheckoutOrder",
         ReleaseLabel='emr-5.26.0',
@@ -89,7 +94,8 @@ def submit_cluster(cluster_seq):
             {
                 'Name': 'ganglia'
             },
-        ],        
+        ],
+        Configurations=configurations,
         Instances=get_instances_descriptions(),      
         Steps=generate_steps(cluster_seq),            
         JobFlowRole='EMR_EC2_DefaultRole',
@@ -104,13 +110,29 @@ def submit_cluster(cluster_seq):
 def _print_config():
     print("Running with following config: \n\
         SCRIPT_NAME:{}, \n\
-        LOG_DEST_S3_URI:{}, \n\
-        DESTINATION_S3_URI_PREFIX:{}, \n"
-        .format(SCRIPT_NAME, LOG_DEST_S3_URI, DESTINATION_S3_URI_PREFIX))
+        LOG_DEST_S3_URI:{}, \n"
+        .format(SCRIPT_NAME, LOG_DEST_S3_URI))
+
+
+def _read_args():
+    parser=argparse.ArgumentParser()
+    parser.add_argument(
+        '--config-path', 
+        help='Local path for json with cluster configuration'
+    )   
+    args=parser.parse_args()
+    return args.config_path
 
 
 if __name__ == "__main__":
     _print_config()
-
-    for cluster_seq in '0123456789ABCDEF':
-        submit_cluster(cluster_seq)
+    
+    config_path = _read_args()
+    
+    config = None
+    if config_path:
+        with open(config_path, 'r') as conf_file:
+            config = json.load(conf_file)
+    
+    for cluster_seq in '46BEF':
+        submit_cluster(cluster_seq, config)
