6fbe2bb7ecdbe9c4879c2d2e39a00609fc798764
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2019-11-06 17:10:37 -0300

    Add script to submit cluster to partition historic Orders data

---
+++ scripts/orders_partition_history/submit_cluster_with_spark_partition_jobs.py
@@ -0,0 +1,155 @@
+import logging
+import boto3
+import os
+import time
+import json
+import argparse
+
+from dotenv import load_dotenv
+
+
+# Load env vars
+load_dotenv('.env')
+SCRIPT_S3_URI = os.getenv('SCRIPT_S3_URI')
+LOG_DEST_S3_URI = os.getenv('LOG_DEST_S3_URI')
+REQUIREMENTS_SCRIPT_S3_URI =  os.getenv(
+    'REQUIREMENTS_SCRIPT_S3_URI',
+    's3://vtex.datalake/scripts/struct_partition_history_orders/install_requirements.sh'
+)
+
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(getattr(logging, os.getenv('LOG_LEVEL', 'INFO')))
+
+
+# AWS Client
+emr_client = boto3.client('emr', region_name='us-east-2')
+
+
+SCRIPT_NAME = SCRIPT_S3_URI.split('/')[-1]
+HOME_HADOOP = '/home/hadoop/'
+
+
+def generate_steps(folder_prefix, destination_path):
+    copy_script_step = {
+        'Name': 'Get partition spark script',
+        'ActionOnFailure': 'TERMINATE_CLUSTER',
+        'HadoopJarStep': {
+            'Jar': 'command-runner.jar',
+            'Args': ['aws', 's3', 'cp', SCRIPT_S3_URI, HOME_HADOOP]
+        }
+    }
+    spark_jobs_steps = [
+        {
+            'Name': 'Run pyspark script for ' + folder_prefix + hexa_prefix,
+            'ActionOnFailure': 'CONTINUE',
+            'HadoopJarStep': {
+                'Jar': 'command-runner.jar',
+                'Args': ['spark-submit', 
+                    '--deploy-mode', 'client',
+                    HOME_HADOOP + SCRIPT_NAME,
+                    '--folder-prefix-filter', str(folder_prefix + hexa_prefix),
+                    '--destination-path', destination_path
+                ]
+            }
+        } for hexa_prefix in '0123456789abcdef'
+    ]
+    steps = [copy_script_step] + spark_jobs_steps
+    return steps
+
+
+def get_instances_descriptions():
+    return {
+        'InstanceGroups': [
+            {
+                'Name': "Master nodes",
+                'Market': 'ON_DEMAND',
+                'InstanceRole': 'MASTER',
+                'InstanceType': 'c5.4xlarge',
+                'InstanceCount': 1,
+            },  
+            {
+                'Name': "Slave nodes",
+                'Market': 'ON_DEMAND',
+                'InstanceRole': 'CORE',
+                'InstanceType': 'c5.4xlarge',
+                'InstanceCount': 2,
+            }               
+        ],
+        'Ec2KeyName': 'dev_datalake',
+        'KeepJobFlowAliveWhenNoSteps': False,
+        'TerminationProtected': False,
+        'Ec2SubnetId': 'subnet-11e84e5d',
+    }
+ 
+
+def submit_cluster(cluster_seq, config_from_json, destination_path):
+    configurations = config_from_json or []
+
+    cluster_id = emr_client.run_job_flow(
+        Name='Partition History Orders --- ' + cluster_seq,
+        ReleaseLabel='emr-5.26.0',
+        Applications=[
+            {
+                'Name': 'Spark'
+            },
+            {
+                'Name': 'ganglia'
+            },
+        ],
+        Configurations=configurations,
+        Instances=get_instances_descriptions(),     
+        BootstrapActions=[
+            {
+                'Name': 'Get install requirements script',
+                'ScriptBootstrapAction': {
+                    'Path': REQUIREMENTS_SCRIPT_S3_URI,
+                }
+            },
+        ],
+        Steps=generate_steps(cluster_seq, destination_path),            
+        JobFlowRole='EMR_EC2_DefaultRole',
+        ServiceRole='EMR_DefaultRole',
+        VisibleToAllUsers=True,
+        LogUri=LOG_DEST_S3_URI,
+    )
+
+    logger.info('Cluster {} created for folder {}*.'.format(cluster_id['JobFlowId'], cluster_seq))
+
+
+def _print_config():
+    print("Running with following config: \n\
+        SCRIPT_NAME:{}, \n\
+        LOG_DEST_S3_URI:{}, \n"
+        .format(SCRIPT_NAME, LOG_DEST_S3_URI))
+
+
+def _read_args():
+    parser=argparse.ArgumentParser()
+    parser.add_argument(
+        '--config-path', 
+        help='Local path for json with cluster configuration',
+        required=False
+    )  
+    parser.add_argument(
+        '--destination-path',
+        help='Where the data to be written. For example: consumable_tables/checkout',
+        required=True
+    )        
+    args=parser.parse_args()
+    return args.config_path, args.destination_path
+
+
+if __name__ == "__main__":
+    _print_config()
+    
+    config_path, destination_path = _read_args()
+    
+    config = None
+    if config_path:
+        with open(config_path, 'r') as conf_file:
+            config = json.load(conf_file)
+    
+    for cluster_seq in '0123456789abcdef':
+        submit_cluster(cluster_seq, config, destination_path)
