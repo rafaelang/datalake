83806aecfbd5d2f21ce92b7e3021ca921f7cde23
Diego Tertuliano <diego.tertuliano@vtex.com>
2019-11-26 17:03:33 -0300

    Save structured streaming version as backup

--- projects/orders-dashboard/src/main/scala/StructuredStreaming.scala
+++ projects/orders-dashboard/src/main/scala/StructuredStreaming.scala
@@ -1,16 +1,18 @@
-import Models.Order
 import org.apache.spark.SparkContext
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.{Column, DataFrame, SparkSession}
+import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.streaming.StreamingQuery
-import org.apache.spark.sql.{DataFrame, SparkSession}
-import org.apache.spark.streaming.{Seconds, StreamingContext}
+import Models.Order
+import Helpers.DataframeTransformations
 
-object Main2 {
+object StructuredStreaming {
 
   def main(args: Array[String]): Unit = {
 
     implicit val sparkSession: SparkSession = SparkSession.builder
-      .master("local[*]")
+      //      .master("local[*]")
       .appName("BlackFridayDashboard")
       .getOrCreate()
 
@@ -20,21 +22,21 @@ object Main2 {
     ssc.sparkContext.setLogLevel("ERROR")
 
     val inputDir: String =
-      "/Users/diego/repos/datalake/projects/orders-dashboard/inputDir/"
+      "s3://vtex.datalake/raw_data/orders/parquet_stage/ingestion_year=*/ingestion_month=*/ingestion_day=*/ingestion_hour=*/"
 
     val outputDir: String =
-      "/Users/diego/repos/datalake/projects/orders-dashboard/outputDir"
+      "s3://vtex-ml/BlackFridayDashboard/Processed/"
 
     val checkpointDir: String =
-      "/Users/diego/repos/datalake/projects/orders-dashboard/checkpoint"
+      "s3://vtex-ml/BlackFridayDashboard/SparkCheckpoint/"
 
     runSparkContext(inputDir: String, outputDir: String, checkpointDir: String)
   }
 
   def runSparkContext(
-      inputDir: String,
-      outputDir: String,
-      checkpointDir: String)(implicit sparkSession: SparkSession): Unit = {
+                       inputDir: String,
+                       outputDir: String,
+                       checkpointDir: String)(implicit sparkSession: SparkSession): Unit = {
 
     import sparkSession.implicits.StringToColumn
 
@@ -43,28 +45,30 @@ object Main2 {
       .option("latestFirst", "true")
       .parquet(inputDir)
 
-//    val explodedDF: DataFrame = dfStream
-//      .withColumn("totals", explode($"totals"))
-//      .withColumn("creationdate", $"creationdate".cast(TimestampType))
-//
-//    val flattenedSchema: Array[Column] =
-//      DataframeTransformations.flattenSchema(explodedDF.schema, None)
-//
-//    val renamedCols: Array[Column] = flattenedSchema.map(name =>
-//      col(name.toString()).as(name.toString().replace(".", "_")))
-//
-//    val outputDF: DataFrame = explodedDF.select(renamedCols: _*)
+    val explodedDF: DataFrame = dfStream
+      .withColumn("totals", explode($"totals"))
+      .withColumn("creationdate", $"creationdate".cast(TimestampType))
 
-    val query: StreamingQuery = dfStream
-      .select(hour($"creationdate")  as "hour",
-              dayofmonth($"creationdate") as "day",
-              month($"creationdate") as "month",
-              year($"creationdate") as "year",
-              $"*")
+    val flattenedSchema: Array[Column] =
+      DataframeTransformations.flattenSchema(explodedDF.schema, None)
+
+    val renamedCols: Array[Column] = flattenedSchema.map(name =>
+      col(name.toString()).as(name.toString().replace(".", "_")))
+
+    val outputDF: DataFrame = explodedDF.select(renamedCols: _*)
+
+    val query: StreamingQuery = outputDF
+      .select(hour($"creationdate") as "hour",
+        dayofmonth($"creationdate") as "day",
+        month($"creationdate") as "month",
+        year($"creationdate") as "year",
+        $"*")
       .writeStream
       .partitionBy("year", "month", "day", "hour")
       .outputMode("append")
-      .format("console")
+      .format("parquet")
+      .option("path", outputDir)
+      .option("checkpointLocation", checkpointDir)
       .start()
 
     query.awaitTermination()
