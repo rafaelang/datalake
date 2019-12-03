aa1ca4a7b74ce1df6ae5ab37ccea42a2c6402656
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2019-12-03 16:22:19 -0300

    Format ScalaStreaming script
    Delete print
    Reindent lines
    Rename vars

--- projects/orders-dashboard/src/main/scala/Main.scala
+++ projects/orders-dashboard/src/main/scala/Main.scala
@@ -1,25 +1,24 @@
+import java.time.format.DateTimeFormatter
+import java.time.{Instant, LocalDateTime, ZoneOffset}
+
 import Helpers.DataframeTransformations
 import org.apache.avro.generic.GenericRecord
 import org.apache.hadoop.fs.Path
 import org.apache.parquet.hadoop.ParquetInputFormat
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.types.TimestampType
 import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
 import org.apache.spark.streaming.dstream.InputDStream
 import org.apache.spark.streaming.{Seconds, StreamingContext, Time}
-import org.apache.spark.sql.functions._
-import java.time.format.DateTimeFormatter
-import java.time.{Instant, LocalDateTime, ZoneOffset}
 
 object Main {
 
-  private val date_formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd/HH")
+  private val date_formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd/HH/mm")
 
   def main(args: Array[String]): Unit = {
 
     implicit val sparkSession: SparkSession = SparkSession.builder
-      .master("local[*]")
       .appName("BlackFridayDashboard")
       .getOrCreate()
 
@@ -29,7 +28,7 @@ object Main {
     ssc.sparkContext.setLogLevel("ERROR")
 
     val inputDir: String =
-      "s3://vtex.datalake/raw_data/orders/parquet_stage/ingestion_year=2019/ingestion_month=11/ingestion_day=28/ingestion_hour=*/"
+      "s3://vtex.datalake/raw_data/orders/parquet_stage/ingestion_year=2019/ingestion_month=12/ingestion_day=*/ingestion_hour=*/"
 
     val outputDir: String =
       "s3://vtex.datalake/test/BlackFridayDashboard/outputDir/"
@@ -38,10 +37,11 @@ object Main {
   }
 
   def runSparkContext(inputDir: String, outputDir: String)(
-      implicit sparkSession: SparkSession,
-      sc: SparkContext,
-      ssc: StreamingContext): Unit = {
+    implicit sparkSession: SparkSession,
+    sc: SparkContext,
+    ssc: StreamingContext): Unit = {
 
+    // In order to properly read parquet data as a fileStream
     ssc.sparkContext.hadoopConfiguration.set(
       "parquet.read.support.class",
       "org.apache.parquet.avro.AvroReadSupport")
@@ -54,13 +54,6 @@ object Main {
         true,
         ssc.sparkContext.hadoopConfiguration)
 
-    stream.foreachRDD { (rdd, time) =>
-      println(rdd.count())
-    }
-
-    val countData = stream.count
-    countData.print
-
     stream
       .map(_._2)
       .transform(rdd => rdd.map(record => record.toString))
@@ -71,23 +64,39 @@ object Main {
         val dataDF: DataFrame = sparkSession.sqlContext.read.json(rdd)
 
         if (!dataDF.isEmpty) {
-          val explodedDF1: DataFrame = dataDF.withColumn("totals", explode($"totals")) //.withColumn("creationdate", $"creationdate".cast(TimestampType))
+          val dfExplodedTotals: DataFrame = dataDF
+            .withColumn("totals", explode($"totals"))
 
-          val explodedDF: DataFrame = explodedDF1.withColumn("LogisticsInfo", explode($"shippingdata.LogisticsInfo"))
+          val dfExplodedLogisticsInfo: DataFrame = dfExplodedTotals
+            .withColumn("LogisticsInfo", explode($"shippingdata.LogisticsInfo"))
 
-          val flattenedSchema: Array[Column] = DataframeTransformations.flattenSchema(explodedDF.schema, None)
+          val flattenedSchema: Array[Column] = DataframeTransformations
+            .flattenSchema(schema = dfExplodedLogisticsInfo.schema, prefix = None)
 
-          val renamedCols: Array[Column] = flattenedSchema.map(name => col(name.toString()).as(name.toString().replace(".", "_")))
+          val renamedCols: Array[Column] = flattenedSchema.map(
+            name =>
+              col(name.toString())
+                .as(name
+                  .toString()
+                  .replace(".", "_")))
 
-          val outputDF: DataFrame = explodedDF.select(renamedCols: _*)
+          val flattenedDF: DataFrame = dfExplodedLogisticsInfo.select(renamedCols: _*)
 
-          val recentlyCreated: DataFrame = outputDF
-            .filter(((col("origin") === 1) && (col("status") === "order-accepted") ) || ((col("origin") === 0) && (col("status") === "order-created")))
+          val recentlyCreated: DataFrame = flattenedDF
+            .filter(
+              ((col("origin") === 1) &&
+                (col("status") === "order-accepted")) ||
+              ((col("origin") === 0) &&
+                (col("status") === "order-created"))
+            )
 
-          val df2: DataFrame = recentlyCreated
-            .withColumn("ordergroup", when(col("origin") === 1, col("orderid")).otherwise(col("ordergroup")))
+          val orderGroupDF: DataFrame = recentlyCreated
+            .withColumn("ordergroup",
+              when(col("origin") === 1,
+                col("orderid")
+              ).otherwise(col("ordergroup")))
 
-          val selectedDf: DataFrame = df2
+          val selectedDF: DataFrame = orderGroupDF
             .select(
               $"origin", $"orderid", $"ordergroup",
               $"LogisticsInfo_array_element_PickupStoreInfo_IsPickupStore",
@@ -97,61 +106,58 @@ object Main {
               $"totals_array_element_value"
             ).distinct()
 
-          val agg_pickup_store_df: DataFrame = selectedDf
+          val aggByPickupStoreDF: DataFrame = selectedDF
             .groupBy(
               $"origin", $"orderid", $"ordergroup",
               $"storepreferencesdata_CountryCode",
               $"storepreferencesdata_CurrencyCode",
               $"hostname", $"value", $"totals_array_element_value",
-              $"totals_array_element_Id"
-            ).agg(max("LogisticsInfo_array_element_PickupStoreInfo_IsPickupStore").alias("isPickupStore"))
+              $"totals_array_element_Id")
+            .agg(max("LogisticsInfo_array_element_PickupStoreInfo_IsPickupStore").alias("isPickupStore"))
 
-          val filtered_shipping_df: DataFrame = agg_pickup_store_df
+          val filteredByShippingDF: DataFrame = aggByPickupStoreDF
             .filter(col("totals_array_element_Id") === "Shipping")
             .select(
               $"origin", $"orderid", $"ordergroup",
               $"storepreferencesdata_CountryCode",
               $"isPickupStore", $"storepreferencesdata_CurrencyCode",
-              $"hostname", $"value", $"totals_array_element_value"
-            )
+              $"hostname", $"value", $"totals_array_element_value")
 
-          val final_df: DataFrame = filtered_shipping_df
+          val totalOrderValueDF: DataFrame = filteredByShippingDF
             .groupBy(
               $"hostname", $"ordergroup", $"origin",
               $"storepreferencesdata_CountryCode",
               $"storepreferencesdata_CurrencyCode"
             ).agg(
-              sum("value").alias("value"),
-              sum("totals_array_element_value").alias("totals_array_element_value"),
-              max("isPickupStore").alias("isPickupStore")
-            )
+            sum("value").alias("value"),
+            sum("totals_array_element_value").alias("totals_array_element_value"),
+            max("isPickupStore").alias("isPickupStore")
+          )
 
-          val final_df2: DataFrame = final_df
-            .withColumn(
-              "is_free_shipping",
+          val addFreeShippingColDF: DataFrame = totalOrderValueDF
+            .withColumn("is_free_shipping",
               when(col("totals_array_element_value") === 0, true)
                 .otherwise(false))
 
-          val final_df3: DataFrame = final_df2
+          val ordersCreated: DataFrame = addFreeShippingColDF
             .groupBy(
-              $"hostname", $"ordergroup", $"origin",
+              $"hostname", $"origin",
               $"storepreferencesdata_CountryCode",
               $"storepreferencesdata_CurrencyCode"
             ).agg(
-              count("ordergroup").alias("orders_number"),
-              sum(col("is_free_shipping").cast("long")).alias("orders_free_shipping"),
-              sum(col("isPickupStore").cast("long")).alias("orders_pickup"),
-              sum("value").alias("revenue")
-            )
+            count("ordergroup").alias("orders_number"),
+            sum(col("is_free_shipping").cast("long")).alias("orders_free_shipping"),
+            sum(col("isPickupStore").cast("long")).alias("orders_pickup"),
+            sum("value").alias("revenue")
+          )
 
-          val dateTime =
-            LocalDateTime.ofInstant(Instant.ofEpochMilli(time.milliseconds),
-                                    ZoneOffset.UTC)
-          final_df3.write
+          val dateTime = LocalDateTime.ofInstant(
+            Instant.ofEpochMilli(time.milliseconds),
+            ZoneOffset.UTC)
+
+          ordersCreated.write
             .mode(SaveMode.Append)
-            .option("delimiter", ";")
-            .option("header", true)
-            .csv(outputDir +
+            .parquet(outputDir +
               dateTime.format(date_formatter))
         }
       })
