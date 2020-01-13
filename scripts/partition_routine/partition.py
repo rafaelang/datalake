f9bf0811276e12ee97d15eb9816e3fd4b544f10d
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2020-01-13 13:37:33 -0300

    Update partition script routine to receive datetime arg

--- scripts/partition_routine/partition.py
+++ scripts/partition_routine/partition.py
@@ -13,16 +13,17 @@ from pytz import timezone
 import sys, os, argparse
 
 
-def get_last_hour_date(timezone_str='UTC'):
-    class MTime(object):
-        def __init__(self, y_str, m_str, d_str, h_str, min_str, s_srt):
-            self.year = y_str
-            self.month = m_str
-            self.day = d_str
-            self.hour = h_str
-            self.min = min_str
-            self.second = s_srt
-    
+class MTime(object):
+    def __init__(self, y_str, m_str, d_str, h_str, min_str, s_srt):
+        self.year = y_str
+        self.month = m_str
+        self.day = d_str
+        self.hour = h_str
+        self.min = min_str
+        self.second = s_srt
+
+
+def get_last_hour_date(timezone_str='UTC'):    
     now = datetime.now(timezone(timezone_str))
     last_hour_date = now - timedelta(hours=1)
     year, month, day = str(last_hour_date).split(' ')[0].split('-')
@@ -32,14 +33,25 @@ def get_last_hour_date(timezone_str='UTC'):
     return last_hour_date
 
 
-def get_dataset_path(datasrc_s3):
-    """Reading dataset to partition (last one hour of data streamed by firehose)"""
-    last_hour_datestr = get_last_hour_date()
+def datetime_str_to_mtime(datetime_str):
+    year, month, day = str(datetime_str).split('_')[0].split('-')
+    hour, minn, second = str(datetime_str).split('_')[1].split(':')
+    return MTime(year, month, day, hour, minn, second)
+
+
+def get_dataset_path(datasrc_s3, datetime_partition=get_last_hour_date()):
+    """
+        Reading dataset to partition (last one hour of data streamed by firehose)
+
+        Args:
+            - datasrc (str):  root path of s3 bucket 
+            - datetime_partition (MTime): get path on s3 based on hour of creation of objects
+    """
     path = datasrc_s3 \
-        + "/ingestion_year=" + str(last_hour_datestr.year) \
-        + "/ingestion_month=" + str(last_hour_datestr.month) \
-        + "/ingestion_day=" + str(last_hour_datestr.day) \
-        + "/ingestion_hour=" + str(last_hour_datestr.hour)
+        + "/ingestion_year=" + str(datetime_partition.year) \
+        + "/ingestion_month=" + str(datetime_partition.month) \
+        + "/ingestion_day=" + str(datetime_partition.day) \
+        + "/ingestion_hour=" + str(datetime_partition.hour)
     return path
 
 
@@ -48,9 +60,11 @@ def getYear(creationDate, lastChange=None):
     date = lastChange if lastChange is not None else creationDate
     return date.split('T')[0].split('-')[0]
 
+
 def getMonth(creationDate, lastChange=None):
     date = lastChange if lastChange is not None else creationDate
     return date.split('T')[0].split('-')[1]
+
 
 def getDay(creationDate, lastChange=None):
     date = lastChange if lastChange is not None else creationDate
@@ -87,17 +101,23 @@ def _read_args():
     parser.add_argument(
         '--use-last-change',
         help='Flag to use or not lastChange date',
-        default='false'
+        default="false"
     )
+    parser.add_argument(
+        '--datetime-partition',
+        help="Partition objects created on this specific date and hour. " \
+            + "If None, current last hour will be used. Format: yyyy-MM-dd_hh:mm:ss",
+        default=None
+    )    
     parser.add_argument('--datasrc-s3')        
     args=parser.parse_args()
     use_last_change = args.use_last_change == "true"
 
-    return args.datasrc_s3, args.destination_path, use_last_change
+    return args.datasrc_s3, args.destination_path, use_last_change, args.datetime_partition
 
 
 if __name__ == "__main__":
-    datasrc_s3, destination_path, use_last_change = _read_args()
+    datasrc_s3, destination_path, use_last_change, datetime_partition = _read_args()
     
     ### Config SparkContext
     spark = SparkSession \
@@ -105,7 +125,12 @@ if __name__ == "__main__":
         .appName("Partition") \
         .getOrCreate()
 
-    datapath = get_dataset_path(datasrc_s3)
+    if datetime_partition:
+        datetime_partition = datetime_str_to_mtime(datetime_partition)
+        datapath = get_dataset_path(datasrc_s3, datetime_partition)
+    else:
+        datapath = get_dataset_path(datasrc_s3)
+        
     df = spark.read.parquet(datapath)
     df = create_partition_columns(df, use_last_change)
 
@@ -116,3 +141,4 @@ if __name__ == "__main__":
         .partitionBy(['YEAR','MONTH','DAY']) \
         .mode('append') \
         .parquet(destination_path)
+    
