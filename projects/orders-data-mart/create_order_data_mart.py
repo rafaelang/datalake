e52828c2575ad3070ddaf559eaeb801288a8d386
Italo Batista <italo.batista@vtex.com>
2020-03-31 17:40:21 -0300

    Add params for exec script for data-mart insertion

--- projects/orders-data-mart/create_order_data_mart.py
+++ projects/orders-data-mart/create_order_data_mart.py
@@ -13,16 +13,9 @@ from pyspark.sql.types import Row
 
 spark = SparkSession.builder.appName('Insert orders data on data warehouse').getOrCreate()
 
-READ_PATH = "../../../data/datalake/raw_data/orders/parquet_stage/ingestion_month=02/ingestion_day=01/ingestion_hour=00"
-df = spark.read.parquet(READ_PATH)
-
-cols = [
-    'id','hostname', 'shippingdata',
-    'items','sellerorderid','ordergroup', 'creationdate',
-    'origin','value','storepreferencesdata', 'iscompleted'
-]
-
-select_df = df.select(*cols)
+YEAR_INDEX = 0
+MONTH_INDEX = 1
+DAY_INDEX = 2
 
 def format_datetime_str(datetime_str):
     datetime_pattern = '%Y-%m-%dT%H:%M:%S.%f'
@@ -30,14 +23,6 @@ def format_datetime_str(datetime_str):
     datetime_object = datetime.strptime(datetime_str_without_UTC, datetime_pattern)
 
     return str(datetime_object.replace(microsecond=0))
-
-format_datetime_str_UDF = UserDefinedFunction(format_datetime_str, StringType())
-select_df = select_df.withColumn('creation_date', format_datetime_str_UDF(select_df.creationdate))
-select_df = select_df.drop("creationdate")
-
-YEAR_INDEX = 0
-MONTH_INDEX = 1
-DAY_INDEX = 2
 
 def getIntervalTime(creationDate, time_index, lastChange=None):
     """
@@ -64,65 +49,102 @@ def create_partition_columns(df, use_last_change):
         df = df.withColumn('DAY', udf_getIntervalTime(df.creation_date, lit(DAY_INDEX)))
     return df
 
-select_df = create_partition_columns(select_df, False)
+def write_csvs(df, write_path):
+    df.repartition('YEAR','MONTH','DAY') \
+        .write \
+        .partitionBy('YEAR','MONTH','DAY') \
+        .format('csv') \
+        .option("sep", ";") \
+        .option("header", "false") \
+        .save('write_path')
 
-select_df = select_df.where(select_df.YEAR >= 2018)
+def _read_args():
+    parser=argparse.ArgumentParser()
+    parser.add_argument(
+        '--s3-read-path',
+        help='S3 path to be read the data. For example: vtex/test',
+        required=True
+    )
+    parser.add_argument(
+        '--s3-destination-path',
+        help='Where the data to be written. For example: vtex/checkout',
+        required=True
+    )
+    args=parser.parse_args()
+    
+    return args.s3_read_path, args.s3_destination_path
 
-select_df = select_df.withColumnRenamed("hostname", "subaccount")
-select_df = select_df.withColumnRenamed("sellerorderid", "seller_order_id")
-select_df = select_df.withColumnRenamed("ordergroup", "order_group")
-select_df = select_df.withColumnRenamed("origin", "origin_code")
-select_df = select_df.withColumnRenamed("value", "total_value")
+## Run PySpark
+if __name__ == "__main__":
+    s3_read_path, s3_destination_path = _read_args()
 
-iscompleted_to_int = when(col("iscompleted") == True, 1).otherwise(0)
-select_df = select_df.withColumn("is_completed", iscompleted_to_int)
+    df = spark.read.parquet(s3_read_path)
 
-select_df = select_df.withColumn("country_code", col("storepreferencesdata.CountryCode"))
-select_df = select_df.withColumn("currency_code", col("storepreferencesdata.CurrencyCode"))
-select_df = select_df.withColumn("number_itens", size(col("items")))
+    cols = [
+        'id','hostname', 'shippingdata',
+        'items','sellerorderid','ordergroup', 'creationdate',
+        'origin','value','storepreferencesdata', 'iscompleted'
+    ]
 
-select_df = select_df.drop("items")
-select_df = select_df.drop("iscompleted")
-select_df = select_df.drop("storepreferencesdata")
+    select_df = df.select(*cols)
 
-pkeys = [
-    'id','subaccount','seller_order_id','order_group'
-]
+    format_datetime_str_UDF = UserDefinedFunction(format_datetime_str, StringType())
+    select_df = select_df.withColumn('creation_date', format_datetime_str_UDF(select_df.creationdate))
+    select_df = select_df.drop("creationdate")
 
-order_item_has_pickup = when(col("PickupStoreInfo.IsPickupStore") == True, 1).otherwise(0)
+    select_df = create_partition_columns(select_df, False)
 
-itmd_df = select_df \
-    .select('id','subaccount','seller_order_id','order_group', explode("shippingdata.LogisticsInfo").alias("LogisticsInfo")) \
-    .select('id','subaccount','seller_order_id','order_group', "LogisticsInfo.PickupStoreInfo", "LogisticsInfo.ItemIndex") \
-    .withColumn("item_with_pickup", order_item_has_pickup) \
-    .select('id','subaccount','seller_order_id','order_group', "item_with_pickup", "ItemIndex") \
-    .groupby('id','subaccount','seller_order_id','order_group', "item_with_pickup") \
-    .agg(countDistinct('ItemIndex').alias('count'))
+    select_df = select_df.where(select_df.YEAR >= 2018)
+
+    select_df = select_df.withColumnRenamed("hostname", "subaccount")
+    select_df = select_df.withColumnRenamed("sellerorderid", "seller_order_id")
+    select_df = select_df.withColumnRenamed("ordergroup", "order_group")
+    select_df = select_df.withColumnRenamed("origin", "origin_code")
+    select_df = select_df.withColumnRenamed("value", "total_value")
+
+    iscompleted_to_int = when(col("iscompleted") == True, 1).otherwise(0)
+    select_df = select_df.withColumn("is_completed", iscompleted_to_int)
+
+    select_df = select_df.withColumn("country_code", col("storepreferencesdata.CountryCode"))
+    select_df = select_df.withColumn("currency_code", col("storepreferencesdata.CurrencyCode"))
+    select_df = select_df.withColumn("number_itens", size(col("items")))
+
+    select_df = select_df.drop("items")
+    select_df = select_df.drop("iscompleted")
+    select_df = select_df.drop("storepreferencesdata")
+
+    pkeys = [
+        'id','subaccount','seller_order_id','order_group'
+    ]
+
+    order_item_has_pickup = when(col("PickupStoreInfo.IsPickupStore") == True, 1).otherwise(0)
+
+    itmd_df = select_df \
+        .select('id','subaccount','seller_order_id','order_group', explode("shippingdata.LogisticsInfo").alias("LogisticsInfo")) \
+        .select('id','subaccount','seller_order_id','order_group', "LogisticsInfo.PickupStoreInfo", "LogisticsInfo.ItemIndex") \
+        .withColumn("item_with_pickup", order_item_has_pickup) \
+        .select('id','subaccount','seller_order_id','order_group', "item_with_pickup", "ItemIndex") \
+        .groupby('id','subaccount','seller_order_id','order_group', "item_with_pickup") \
+        .agg(countDistinct('ItemIndex').alias('count'))
 
 
-itmd_df_pickup = itmd_df.where(col("item_with_pickup") == 1)
-itmd_df_pickup = itmd_df_pickup.withColumnRenamed("count", "pickup_itens")
-itmd_df_pickup = itmd_df_pickup.drop("item_with_pickup")
+    itmd_df_pickup = itmd_df.where(col("item_with_pickup") == 1)
+    itmd_df_pickup = itmd_df_pickup.withColumnRenamed("count", "pickup_itens")
+    itmd_df_pickup = itmd_df_pickup.drop("item_with_pickup")
 
-itmd_df = itmd_df.drop("count", "item_with_pickup")
-itmd_df = itmd_df.dropDuplicates()
+    itmd_df = itmd_df.drop("count", "item_with_pickup")
+    itmd_df = itmd_df.dropDuplicates()
 
-joined = itmd_df.join(itmd_df_pickup, on=pkeys, how='left')
+    joined = itmd_df.join(itmd_df_pickup, on=pkeys, how='left')
 
-joined = joined.na.fill(0)
+    joined = joined.na.fill(0)
 
-select_df = select_df.drop('shippingdata')
+    select_df = select_df.drop('shippingdata')
 
-orders = select_df.join(joined, on=pkeys, how='left')
+    orders = select_df.join(joined, on=pkeys, how='left')
 
-pickup_null_to_zero = when(col("pickup_itens").isNull(), 0).otherwise(col("pickup_itens"))
-orders = orders.withColumn("pickup_itens", pickup_null_to_zero)
-orders = orders.dropDuplicates(subset = ['id','subaccount','seller_order_id','order_group'])
+    pickup_null_to_zero = when(col("pickup_itens").isNull(), 0).otherwise(col("pickup_itens"))
+    orders = orders.withColumn("pickup_itens", pickup_null_to_zero)
+    orders = orders.dropDuplicates(subset = ['id','subaccount','seller_order_id','order_group'])
 
-orders.repartition('YEAR','MONTH','DAY') \
-    .write \
-    .partitionBy('YEAR','MONTH','DAY') \
-    .format('csv') \
-    .option("sep", ";") \
-    .option("header", "false") \
-    .save('datamart_csv')
+    write_csvs(orders, s3_destination_path)
